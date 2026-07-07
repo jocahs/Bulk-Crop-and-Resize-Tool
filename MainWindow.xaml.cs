263192af5018b30565ac9887ed872e9f66f07835
jocahs <jocahs.jocahs@gmail.com>
2026-07-07 18:22:42 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace ImageCropTool
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SrcBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                CheckFileExists = false,
+                ValidateNames = false,
+                FileName = " ",
+                Filter = "Image files|*.jpg;*.jpeg;*.png;*.bmp;*.gif"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                SrcBox.Text = dialog.FileName;
+            }
+        }
+
+        private void NoOverwriteChk_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ModePrefix_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
