4b900877ab79f6b1c9ef0c4e351ca94ccd295da0
jocahs <jocahs.jocahs@gmail.com>
2026-07-18 00:31:00 +0100

    Add files via upload

---
+++ Helpers/AppConstants.cs
@@ -0,0 +1,39 @@
+namespace BulkCropAndResizeTool.Helpers
+{
+public static class AppConstants
+{
+    #region Default Text
+
+    public const string DefaultSrcBoxText = "Write/Paste or Browse the source path of a file/folder ----->";
+    public const string DefaultDstBoxText = "Write/Paste or Browse if different from Source folder ------>";
+
+    public const string DefaultFilename = "filename";
+    public const string DefaultExtension = ".jpg";
+
+    #endregion
+
+    #region Filename Prefixes
+
+    public const string DefaultCropPrefix = "cropped_";
+    public const string DefaultCropSuffix = "_cropped";
+
+    public const string DefaultResizePrefix = "resized_";
+    public const string DefaultResizeSuffix = "_resized";
+
+    #endregion
+
+    #region Image Types
+
+    public static readonly string[] ImageExtensions =
+    {
+        "*.jpg",
+        "*.jpeg",
+        "*.png",
+        "*.bmp",
+        "*.gif",
+        "*.tiff"
+    };
+
+        #endregion
+    }
+}
